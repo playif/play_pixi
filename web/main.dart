3146072ccdf6069e8c9718372b6f101e92c7a4d4
playif <playif@gmail.com>
2014-07-08 11:54:35 +0800

    1.5.0 batch

---
+++ web/main.dart
@@ -0,0 +1,48 @@
+import "dart:html";
+import "dart:math";
+import "package:pixi_dart/pixi.dart" as PIXI;
+
+main() {
+  // create an new instance of a pixi stage
+  var stage = new PIXI.Stage(0x660099);
+
+  // create a renderer instance
+  //var renderer =  new PIXI.CanvasRenderer(400, 300);
+  //var renderer = new PIXI.WebGLRenderer(400, 300);
+  var renderer = PIXI.autoDetectRenderer(400, 300);
+
+  // add the renderer view element to the DOM
+  document.body.append(renderer.view);
+
+
+  // create a texture from an image path
+  var texture = PIXI.Texture.fromImage("bunny.png");
+  //window.console.log(texture.baseTexture);
+  // create a new Sprite using the texture
+  var bunny = new PIXI.Sprite(texture);
+
+  Random random = new Random();
+  bunny.tint = random.nextInt(0xFFFFFF);
+
+  // center the sprites anchor point
+  bunny.anchor.x = 0.5;
+  bunny.anchor.y = 0.5;
+
+  // move the sprite t the center of the screen
+  bunny.position.x = 200;
+  bunny.position.y = 150;
+
+  stage.addChild(bunny);
+
+  animate(num delta) {
+    PIXI.requestAnimFrame(animate);
+
+    // just for fun, let's rotate mr rabbit a little
+    bunny.rotation += 0.1;
+
+    // render the stage
+    renderer.render(stage);
+  }
+  PIXI.requestAnimFrame(animate);
+
+}
