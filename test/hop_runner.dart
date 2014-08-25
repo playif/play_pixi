3e74e2b25cbd3231d4955455f2c2b1729bde7207
playif <playif@gmail.com>
2014-08-25 13:27:11 +0800

    start add test

---
+++ test/hop_runner.dart
@@ -0,0 +1,22 @@
+library dumprendertree;
+
+import 'package:hop/hop.dart';
+import 'dart:io';
+import 'dart:async';
+
+main(List<String> args) {
+  addTask('test', createUnitTestTask());
+  runHop(args);
+}
+
+Task createUnitTestTask() {
+  return new Task((TaskContext tcontext) {
+    tcontext.info("Running Unit Tests....");
+    var result = Process.run('./content_shell',
+    ['--dump-render-tree','test/test.html'])
+    .then((ProcessResult process) {
+      tcontext.info(process.stdout);
+    });
+    return result;
+  });
+}
